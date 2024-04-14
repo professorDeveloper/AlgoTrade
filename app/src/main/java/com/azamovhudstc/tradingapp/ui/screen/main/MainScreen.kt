21bd6b2afd080424c2e8ab07dbb02a3ea461ea6f
professorDeveloper <azamovhud007@outlook.com>
2024-04-14 17:32:56 +0500

    Init Main

---
+++ app/src/main/java/com/azamovhudstc/tradingapp/ui/screen/main/MainScreen.kt
@@ -0,0 +1,17 @@
+package com.azamovhudstc.tradingapp.ui.screen.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.azamovhudstc.tradingapp.R
+import com.azamovhudstc.tradingapp.base.BaseFragment
+import com.azamovhudstc.tradingapp.databinding.MainScreenBinding
+
+class MainScreen : BaseFragment<MainScreenBinding>(MainScreenBinding::inflate) {
+    override fun onViewCreate() {
+    }
+
+
+}
