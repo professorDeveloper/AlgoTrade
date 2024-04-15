a6b367e9f153d2c9f5aadbe121eb50772c8f2fec
professorDeveloper <azamovhud007@outlook.com>
2024-04-15 23:16:14 +0500

    Add Coordinator for HomeScreen

--- app/src/main/java/com/azamovhudstc/tradingapp/local/LocalHome.kt
+++ app/src/main/java/com/azamovhudstc/tradingapp/local/LocalHome.kt
@@ -2,6 +2,7 @@ package com.azamovhudstc.tradingapp.local
 
 import com.azamovhudstc.tradingapp.R
 import com.azamovhudstc.tradingapp.data.DiveItem
+import com.azamovhudstc.tradingapp.data.MarketItem
 import com.azamovhudstc.tradingapp.data.TrendItem
 
 object LocalHome {
@@ -26,6 +27,156 @@ object LocalHome {
         list.add(TrendItem(R.drawable.tesla, "Tesla", "\$ 341.65", "+6.6 %", "#E82126"))
         list.add(TrendItem(R.drawable.nvidia, "Nvidia", "\$ 591.67", "+9.4 %", "#94C11E"))
         list.add(TrendItem(R.drawable.meta, "Meta", "$ 421.89", "+6.3 %", "#FEC107"))
+        list.add(TrendItem(R.drawable.tesla, "Tesla", "\$ 341.65", "+6.6 %", "#E82126"))
+        list.add(TrendItem(R.drawable.tesla, "Tesla", "\$ 341.65", "+6.6 %", "#E82126"))
+        list.add(TrendItem(R.drawable.nvidia, "Nvidia", "\$ 591.67", "+9.4 %", "#94C11E"))
+        list.add(TrendItem(R.drawable.meta, "Meta", "$ 421.89", "+6.3 %", "#FEC107"))
+        list.add(TrendItem(R.drawable.tesla, "Tesla", "\$ 341.65", "+6.6 %", "#E82126"))
+        list.add(TrendItem(R.drawable.nvidia, "Nvidia", "\$ 591.67", "+9.4 %", "#94C11E"))
+        list.add(TrendItem(R.drawable.meta, "Meta", "$ 421.89", "+6.3 %", "#FEC107"))
+        list.add(TrendItem(R.drawable.tesla, "Tesla", "\$ 341.65", "+6.6 %", "#E82126"))
+        list.add(TrendItem(R.drawable.nvidia, "Nvidia", "\$ 591.67", "+9.4 %", "#94C11E"))
+        list.add(TrendItem(R.drawable.meta, "Meta", "$ 421.89", "+6.3 %", "#FEC107"))
+        return list
+    }
+
+    fun loadMarketList(): ArrayList<MarketItem> {
+        val list = ArrayList<MarketItem>()
+        list.add(
+            MarketItem(
+                "ETH",
+                R.drawable.ethereum,
+                "Tether Ethereum",
+                "\$ 3,101.52",
+                "−44.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "BIT",
+                R.drawable.bitcoin,
+                "Tether Bitcoin",
+                "\$ 56 348 01",
+                "+ 7,34 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "USD",
+                R.drawable.usd,
+                "Tether USD",
+                "\$ 76 458 00",
+                "+14.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "BNB",
+                R.drawable.bnb,
+                "Tether BNB",
+                "\$ 56 458 00",
+                "-8.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "DOGE",
+                R.drawable.dogecoin,
+                "Tether DOGE",
+                "\$ 1,939.13",
+                "-104.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "CSM",
+                R.drawable.cosmos,
+                "Tether CMS",
+                "\$ 6 458 00",
+                "+34.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "LUNC",
+                R.drawable.crypto,
+                "Tether CMS",
+                "\$  4 00",
+                "-14.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "BNB",
+                R.drawable.bnb,
+                "Tether BNB",
+                "\$ 56 458 00",
+                "-8.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "DOGE",
+                R.drawable.dogecoin,
+                "Tether DOGE",
+                "\$ 1,939.13",
+                "-104.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "ETH",
+                R.drawable.ethereum,
+                "Tether Ethereum",
+                "\$ 3,101.52",
+                "−44.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "BIT",
+                R.drawable.bitcoin,
+                "Tether Bitcoin",
+                "\$ 56 348 01",
+                "+ 7,34 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "USD",
+                R.drawable.usd,
+                "Tether USD",
+                "\$ 76 458 00",
+                "+14.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "BNB",
+                R.drawable.bnb,
+                "Tether BNB",
+                "\$ 56 458 00",
+                "-8.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "DOGE",
+                R.drawable.dogecoin,
+                "Tether DOGE",
+                "\$ 1,939.13",
+                "-104.12 %"
+            )
+        )
+        list.add(
+            MarketItem(
+                "CSM",
+                R.drawable.cosmos,
+                "Tether CMS",
+                "\$ 6 458 00",
+                "+34.12 %"
+            )
+        )
         return list
     }
 }
