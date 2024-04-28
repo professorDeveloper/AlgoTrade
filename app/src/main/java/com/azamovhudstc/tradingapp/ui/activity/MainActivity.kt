ea0e15242c0a1c1e8d511053dded487a3134578a
professorDeveloper <azamovhud007@outlook.com>
2024-04-29 00:51:34 +0500

    Added Languages

--- app/src/main/java/com/azamovhudstc/tradingapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/azamovhudstc/tradingapp/ui/activity/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.azamovhudstc.tradingapp.ui.activity
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.azamovhudstc.tradingapp.R
 import com.azamovhudstc.tradingapp.tools.initActivity
+import com.prongbang.localization.LocalizationAppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : LocalizationAppCompatActivity  () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initActivity(this)
     }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        block?.invoke()
+        openPrepareLocalize()
+        super.onConfigurationChanged(newConfig)
+    }
+
+    fun mySetLocate(locale: Locale, _block: (() -> Unit)) {
+        block = _block
+        setLocale(locale)
+    }
+
+    private var block: (() -> Unit)? = null
 }
--- app/src/main/java/com/azamovhudstc/tradingapp/ui/screen/profile/sheet/LanguageDialogSheet.kt
+++ app/src/main/java/com/azamovhudstc/tradingapp/ui/screen/profile/sheet/LanguageDialogSheet.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.azamovhudstc.tradingapp.databinding.ChangeLanguageBinding
+import com.azamovhudstc.tradingapp.ui.activity.MainActivity
+import com.azamovhudstc.tradingapp.utils.loadData
+import com.azamovhudstc.tradingapp.utils.saveData
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
 
 class LanguageDialogSheet : BottomSheetDialogFragment() {
     private var _binding: ChangeLanguageBinding? = null
     private val binding get() = _binding!!
 
+    private var lang = loadData<String>("lang") ?: "uz"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,12 +30,45 @@ class LanguageDialogSheet : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    private fun setLocate(language: String) {
+        val activity = (requireActivity() as MainActivity)
+        activity.mySetLocate(Locale.forLanguageTag(language)) {
+            lifecycleScope.launch {
+                delay(300)
+            }
+        }
+    }
+
     @SuppressLint("CutPasteId")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+
+            binding.saveLanguageBtn.setOnClickListener {
+                setLocate(lang)
+                saveData("lang", lang)
+                dismiss()
+            }
+
+            when (lang) {
+                "uz" -> {
+                    uzbekBtn.isChecked = true
+                }
+                "eng" -> {
+                    englishBtn.isChecked = true
+
+                }
+                "ru" -> {
+                    russianBtn.isChecked = true
+                }
+                "in" -> {
+                    indianBtn.isChecked = true
+                }
+            }
+
             uzbekBtn.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
+                    lang = "uz"
                     englishBtn.isChecked = false
                     russianBtn.isChecked = false
                     indianBtn.isChecked = false
@@ -36,12 +77,14 @@ class LanguageDialogSheet : BottomSheetDialogFragment() {
             englishBtn.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
                     uzbekBtn.isChecked = false
+                    lang = "eng"
                     russianBtn.isChecked = false
                     indianBtn.isChecked = false
                 }
             }
             russianBtn.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
+                    lang = "ru"
 
                     englishBtn.isChecked = false
                     uzbekBtn.isChecked = false
@@ -51,12 +94,13 @@ class LanguageDialogSheet : BottomSheetDialogFragment() {
 
             indianBtn.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
-
                     englishBtn.isChecked = false
                     russianBtn.isChecked = false
                     uzbekBtn.isChecked = false
+                    lang = "in"
                 }
             }
+
         }
     }
 
